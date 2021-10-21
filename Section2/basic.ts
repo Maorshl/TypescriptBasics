6f8c43e8682ad49ecf6956c9ac4f20e160c9082e
Maorshl <maors@moveo.co.il>
2021-10-21 09:37:44 +0300

    Section2 21

--- Section2/basic.ts
+++ Section2/basic.ts
@@ -1,1 +1,15 @@
-let input1: number;
+const combine = (num1: number | string, num2: number | string) => {
+  let result;
+  if (typeof num1 === "number" && typeof num2 === "number") {
+    result = num1 + num2;
+  } else {
+    result = num1.toString() + num2.toString();
+  }
+  return result;
+};
+
+const combinedAges = combine(30, 26);
+console.log(combinedAges);
+
+const combinedNames = combine("Max", "Anna");
+console.log(combinedNames);
